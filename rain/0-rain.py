29a3707e6b39c4b8d54cffca3918a81b1d606569
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-09-28 14:24:07 +0200

    hey hey

---
+++ rain/0-rain.py
@@ -0,0 +1,43 @@
+"""calculating total amount of rainwater kept after it rains"""
+def rain(walls):
+    """
+    Assumption:
+    The ends of the list  are not walls,
+    meaning they will not retain water.
+    """
+
+    #checking if input list is empy
+    if not walls:
+        return 0
+
+    # Initialise two pointers for both ends of list & tracking max heights
+    left, right = 0, len(walls) - 1
+    left_max, right_max = walls[left], walls[right]
+    water_retained = 0
+
+    #Balancing their heights before they reach same level.
+    while left < right:
+        if left_max < right_max:
+            left += 1
+
+            # Update left_max if other wall taller
+            left_max = max(left_max, walls[left])
+
+            # Calculate water retained above current wall
+            water_retained += left_max - walls[left]
+        else:
+            #Move the right pointer towards the left
+            right -= 1
+            right_max = max(right_max, walls[right])
+            #Calculate the water retained above the current wall
+            water_retained += right_max - walls[right]
+
+    return water_retained
+
+
+if __name__ == "__main__":
+    #Uses algorithm above to actually calculate example
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))
---
+++ rain/README.md
@@ -0,0 +1,6 @@
+# Rain
+Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains.
+
+This project calculates amount of rainwater that can be retained, given a list of non-negative integers representing the heights of walls. 
+
+Each unit of height corresponds to a wall of unit width 1, simulating a cross-section of a relief map.
