2b66d35d6d6dea7dbbdbb18bf3216ff7f063fafa
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-09-28 16:36:22 +0200

    here we go

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """calculating total amount of rainwater kept after it rains"""
+
+
 def rain(walls):
     """
     Assumption:
@@ -7,7 +9,7 @@ def rain(walls):
     meaning they will not retain water.
     """
 
-    #checking if input list is empy
+    # checking if input list is empy
     if not walls:
         return 0
 
@@ -16,7 +18,7 @@ def rain(walls):
     left_max, right_max = walls[left], walls[right]
     water_retained = 0
 
-    #Balancing their heights before they reach same level.
+    # Balancing their heights before they reach same level.
     while left < right:
         if left_max < right_max:
             left += 1
@@ -27,17 +29,17 @@ def rain(walls):
             # Calculate water retained above current wall
             water_retained += left_max - walls[left]
         else:
-            #Move the right pointer towards the left
+            # Move the right pointer towards the left
             right -= 1
             right_max = max(right_max, walls[right])
-            #Calculate the water retained above the current wall
+            # Calculate the water retained above the current wall
             water_retained += right_max - walls[right]
 
     return water_retained
 
 
 if __name__ == "__main__":
-    #Uses algorithm above to actually calculate example
+    # Uses algorithm above to actually calculate example
     walls = [0, 1, 0, 2, 0, 3, 0, 4]
     print(rain(walls))
     walls = [2, 0, 0, 4, 0, 0, 1, 0]
